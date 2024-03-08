402424bf7ff54ac8eabeefd27c2f146a8f5a07bf
obadiahokello <145970565+obadiahokello@users.noreply.github.com>
2024-03-08 04:26:01 +0300

    Merge remote-tracking branch 'origin/master'

--- Moru_safaris/build_files.sh
+++ Moru_safaris/build_files.sh
@@ -1,5 +1,4 @@
 pip install --upgrade pip
 pip install -r requirements.txt
-python3 -m venv my_project_env
-source my_project_env/bin/activate
-Python manage.py collectstatic
+
+python manage.py collectstatic
